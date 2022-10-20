3dc8b05ce39e126215921e5cc6e56b8e81bb9e5f
moxeed <mortezabahjat@gmail.com>
2022-10-20 12:22:42 +0330

    Client ID And Save Close Reason

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func main() {
 	docs.SwaggerInfo.Schemes = common.Configuration.Schemes
 	docs.SwaggerInfo.Host = common.Configuration.Host
 	docs.SwaggerInfo.BasePath = common.Configuration.BasePath
+	docs.SwaggerInfo.Version = "1.0.0"
 
 	gin.SetMode(gin.ReleaseMode)
 
